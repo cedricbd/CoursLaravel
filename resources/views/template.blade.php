4525053d3943efe9b632fdd0cb44b329f8ba4ae1
cedricbd <cebuisso@gmail.com>
2019-04-25 17:49:26 +0200

    template contact

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+{{-- <!doctype html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
@@ -6,4 +6,21 @@
 <body>
     @yield('contenu')
 </body>
+</html> --}}
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Mon joli site</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <style>
+            textarea { resize: none; }
+            .card { width: 25em; }
+        </style>
+    </head>
+    <body>
+        @yield('contenu')
+    </body>
 </html>
