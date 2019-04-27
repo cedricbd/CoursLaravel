a95e21089da27abab38393b476012bee0b263d4d
cedricbd <cebuisso@gmail.com>
2019-04-27 11:30:19 +0200

    add  view confirm

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function create() 
+    {
+        return view('contact');
+    }
+
+    public function store() 
+    {
+        return view('confirm');
+    }
 }
---
+++ resources/views/errors/confirm.blade.php
@@ -0,0 +1,12 @@
+@extends('template')
+
+@section('contenu')
+    <br>
+    <div class="container">
+        <div class="row card text-white bg_dark">
+            <h4 class=card-header>Contactez-moi</h4>
+            <div class="card-body">
+                <p class="card-text">Merci. Votre message à été transmis à l'administrateur du site. Vous recevrez une réponse raipdement.</p>
+            </div>
+        </div>
+    </div>
